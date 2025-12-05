f2581bb2f0d5cb91fe6168697af2d391e0e68460
ehla112 <dguamanrincon@outlook.com>
2025-12-04 22:01:16 -0500

    templatemo.js updated

--- assets/landing/js/templatemo.js
+++ assets/landing/js/templatemo.js
@@ -6,52 +6,57 @@ https://templatemo.com/tm-559-zay-shop
 
 */
 
+
 'use strict';
-$(document).ready(function() {
+
+$(document).ready(function () {
 
     // Accordion
-    var all_panels = $('.templatemo-accordion > li > ul').hide();
+    const all_panels = $('.templatemo-accordion > li > ul').hide();
 
-    $('.templatemo-accordion > li > a').click(function() {
+    $('.templatemo-accordion > li > a').click(function () {
         console.log('Hello world!');
-        var target =  $(this).next();
-        if(!target.hasClass('active')){
+        const target = $(this).next();
+        if (!target.hasClass('active')) {
             all_panels.removeClass('active').slideUp();
             target.addClass('active').slideDown();
         }
-      return false;
+        return false;
     });
     // End accordion
 
     // Product detail
-    $('.product-links-wap a').click(function(){
-      var this_src = $(this).children('img').attr('src');
-      $('#product-detail').attr('src',this_src);
-      return false;
+    $('.product-links-wap a').click(function () {
+        const this_src = $(this).children('img').attr('src');
+        $('#product-detail').attr('src', this_src);
+        return false;
     });
-    $('#btn-minus').click(function(){
-      var val = $("#var-value").html();
-      val = (val=='1')?val:val-1;
-      $("#var-value").html(val);
-      $("#product-quanity").val(val);
-      return false;
+
+    $('#btn-minus').click(function () {
+        let val = $("#var-value").html();
+        val = (val === '1') ? val : val - 1;
+        $("#var-value").html(val);
+        $("#product-quanity").val(val);
+        return false;
     });
-    $('#btn-plus').click(function(){
-      var val = $("#var-value").html();
-      val++;
-      $("#var-value").html(val);
-      $("#product-quanity").val(val);
-      return false;
+
+    $('#btn-plus').click(function () {
+        let val = $("#var-value").html();
+        val++;
+        $("#var-value").html(val);
+        $("#product-quanity").val(val);
+        return false;
     });
-    $('.btn-size').click(function(){
-      var this_val = $(this).html();
-      $("#product-size").val(this_val);
-      $(".btn-size").removeClass('btn-secondary');
-      $(".btn-size").addClass('btn-success');
-      $(this).removeClass('btn-success');
-      $(this).addClass('btn-secondary');
-      return false;
+
+    $('.btn-size').click(function () {
+        const this_val = $(this).html();
+        $("#product-size").val(this_val);
+        $(".btn-size").removeClass('btn-secondary');
+        $(".btn-size").addClass('btn-success');
+        $(this).removeClass('btn-success');
+        $(this).addClass('btn-secondary');
+        return false;
     });
-    // End roduct detail
+    // End product detail
 
 });
