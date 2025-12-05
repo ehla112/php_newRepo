7b7c658b751b230909ea97378924478e54cb5963
ehla112 <dguamanrincon@outlook.com>
2025-12-04 21:41:54 -0500

    index updated

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
     ob_start();
     session_start();
     // session_destroy();
+     session_start();
     require_once "models/DataBase.php";
-    session_start();
+   
     //Controladores permitidos
     $allowedControllers =[
         'Landing',
@@ -11,18 +12,25 @@
         'Company',
         'Admin'
     ];
-    $controller = isset($_REQUEST['c']) ? $_REQUEST['c'] : "Landing";
-    if(!in_array($controller,$allowedControllers)){
-        $controller = 'Landing';
+    $controllerName = isset($_REQUEST['c']) ? $_REQUEST['c'] : "Landing";
+    if(!in_array($controllerName,$allowedControllers)){
+        $controllerName = 'Landing';
     }
-    $route_controller = "controllers/" . $controller . ".php";
-    if (file_exists($route_controller)) {
-        require_once $route_controller;
-    }else{
-        require_once "controllers/Landing.php";
+    $controllersMap = [
+    'Landing' => 'controllers/Landing.php',
+    'User'    => 'controllers/User.php',
+    'Company' => 'controllers/Company.php',
+    'Admin'   => 'controllers/Admin.php'
+    
+    ];
+    
+    require_once $controllersMap[$controllerName];
+
+    $controller = new $controllerName;
+
+    if (!method_exists($controller, $action)) {
+    $action = 'main';
     }
-        $controller = new $controller;
-        $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
-        call_user_func([$controller,$action]);
+    call_user_func([$controller, $action]); 
     
 ?>
