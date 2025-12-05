c5aa0c67141cbf91c068fe356c235dde1d3f2ab6
ehla112 <dguamanrincon@outlook.com>
2025-12-04 21:19:38 -0500

    changes index

--- index.php
+++ index.php
@@ -3,29 +3,26 @@
     session_start();
     // session_destroy();
     require_once "models/DataBase.php";
+    session_start();
+    //Controladores permitidos
+    $allowedControllers =[
+        'Landing',
+        'User',
+        'Company',
+        'Admin'
+    ];
     $controller = isset($_REQUEST['c']) ? $_REQUEST['c'] : "Landing";
+    if(!in_array($controller,$allowedControllers)){
+        $controller = 'Landing';
+    }
     $route_controller = "controllers/" . $controller . ".php";
     if (file_exists($route_controller)) {
-        $view = $controller;
         require_once $route_controller;
+    }else{
+        require_once "controllers/Landing.php";
+    }
         $controller = new $controller;
         $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
-        if ($view === 'Landing' || $view === 'Login') {
-            require_once "views/company/header.view.php";
-            call_user_func(array($controller, $action));
-            require_once "views/company/footer.view.php";
-        } elseif (!empty($_SESSION['session'])) {
-            require_once "models/User.php";
-            $profile = unserialize($_SESSION['profile']);
-            $session = $_SESSION['session'];
-            require_once "views/roles/".$session."/header.view.php";
-            call_user_func(array($controller, $action));
-            require_once "views/roles/".$session."/footer.view.php";
-        } else {
-            header("Location:?");
-        }
-    } else {
-        header("Location:?");
-    }
-    ob_end_flush();
+        call_user_func([$controller,$action]);
+    
 ?>
